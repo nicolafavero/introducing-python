4aaa63dbb6dd20a40526782f1f1fa590cce381d6
nicolafavero <61919460+nicolafavero@users.noreply.github.com>
2020-09-11 18:34:41 +0200

    Using os.path.dirname()

--- 1st_edition/art/matplotlib1.py
+++ 1st_edition/art/matplotlib1.py
@@ -3,7 +3,8 @@ import matplotlib.image as image
 import os
 
 
-strPath = os.getcwd() + "/1st_edition/art/"
-img = image.imread(strPath + "oreilly.png")
+#strPath = os.getcwd() + "/1st_edition/art/"
+strPath = os.path.dirname(__file__)
+img = image.imread(strPath + "/oreilly.png")
 plot.imshow(img)
 plot.show()
